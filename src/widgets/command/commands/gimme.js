097ca9be140ff1b8fd05563e26d88e26ca493210
Bryan Wong <bryanw0ng@ucla.edu>
2021-07-21 00:16:05 -0700

    add handling

--- src/widgets/command/commands/gimme.js
+++ src/widgets/command/commands/gimme.js
@@ -41,6 +41,10 @@ const searchCache = new NodeCache({ stdTTL: 0, checkperiod: 0 });
 
 const QUERY_LIMIT = 500;
 
+let scraperJobs = {};
+
+const executeQuery = new Promise((resolve, reject) => {});
+
 module.exports = new CommandBuilder()
   .setAliases(["g", "gimme"])
   .setOwnersOnly(false)
@@ -54,17 +58,52 @@ module.exports = new CommandBuilder()
     const query = args.join(" ");
     let results;
     if (searchCache.get(query) == null) {
-      await message.channel.send(
-        "<:helloeverybunny:837449515559551056> Please wait, I'm getting you your " +
-          query +
-          " (can take up to a minute if it's your first time searching for this)"
-      );
-      results = await google.scrape(query, QUERY_LIMIT);
+      if (Object.keys(scraperJobs).length > 2) {
+        const index = Math.floor(
+          Object.keys(scraperJobs).length * Math.random()
+        );
+        const scraperJobQuery = Object.keys(scraperJobs)[index];
+        const scraperJobUser = scraperJobs[index];
+        await message.channel.send(
+          "I'm busy getting " +
+            scraperJobQuery +
+            " for " +
+            scraperJobUser +
+            ". Please try again in a few."
+        );
+        return;
+      }
+      if (scraperJobs[query] != null) {
+        await message.channel.send(
+          "You already asked me for " +
+            query +
+            "! Be patient " +
+            user.username +
+            " 😠"
+        );
+        return;
+      } else {
+        scraperJobs[query] = user.username;
+        await message.channel.send(
+          "<:helloeverybunny:837449515559551056> Please wait, I'm getting you your " +
+            query +
+            " (can take up to a minute if it's your first time searching for this)"
+        );
+        google
+          .scrape(query, QUERY_LIMIT)
+          .then((results) => {
+            message.channel.send(
+              results[Math.floor(Math.random() * results.length)].url
+            );
+            delete scraperJobs[query];
+          })
+          .catch((e) => {
+            console.log(e);
+            delete scraperJobs[query];
+          });
+      }
       searchCache.set(query, results);
     } else {
       results = searchCache.get(query);
     }
-    await message.channel.send(
-      results[Math.floor(Math.random() * results.length)].url
-    );
   });
